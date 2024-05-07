7704f8164b8a803925229a53a660c9273ce209a8
subhamc88 <subham020304@gmail.com>
2024-05-07 18:45:56 +0530

    cloak deleted and git added in neovim

--- .config/nvim/lua/subham/plugins/git.lua
+++ .config/nvim/lua/subham/plugins/git.lua
@@ -4,16 +4,16 @@ return {
 		config = function()
 			vim.keymap.set("n", "<leader>ga", ":Git add", {})
 			vim.keymap.set("n", "<leader>gc", ":Git commit", {})
-			vim.keymap.set("n", "<leader>gs", ":Git status", {})
-			vim.keymap.set("n", "<leader>gpu", ":Git push", {})
-			vim.keymap.set("n", "<leader>gpl", ":Git,pull", {})
+			vim.keymap.set("n", "<leader>gs", ":Git status<CR>", {})
+			vim.keymap.set("n", "<leader>gpu", ":Git push<CR>", {})
+			vim.keymap.set("n", "<leader>gpl", ":Git pull<CR>", {})
 		end,
 	},
 	{
 		"lewis6991/gitsigns.nvim",
 		config = function()
 			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>gpp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gph", ":Gitsigns preview_hunk<CR>", {})
 		end,
 	},
 }
