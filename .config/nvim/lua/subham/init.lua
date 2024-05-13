285b4454fd325c232ea4ea97d534632c595656a8
subhamc88 <subham020304@gmail.com>
2024-05-13 10:55:20 +0530

    nvim file deleted for making a new one

--- .config/nvim/lua/subham/init.lua
+++
@@ -1,3 +0,0 @@
-require("subham.lazy")
-require("subham.keymaps")
-require("subham.options")
--- .config/nvim/lua/subham/plugins/nvim-dap.lua
+++
@@ -1,9 +0,0 @@
-return{
-    "mfussenegger/nvim-dap",
-    dependencies={"rcarriga/nvim-dap-ui"},
-    config = function ()
-        local dap = require("dap")
-        vim.keymap.set('n','<leader>dt',dap.toggle_breakpoint, {})
-        vim.keymap.set('n','<leader>dc',dap.continue,{})
-    end
-}
