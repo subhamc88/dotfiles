1dc5011526821fabc3f5f0d024aeac8e1378c0cf
subhamc8 <subham020304@gmail.com>
2024-05-18 00:53:35 +0530

    minicursorword and minianimate added to neovim

--- .config/nvim/lua/subham/plugins/mini.lua
+++ .config/nvim/lua/subham/plugins/mini.lua
@@ -2,12 +2,6 @@ return {
   'echasnovski/mini.nvim',
   config = function()
     require('mini.surround').setup()
-    local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = vim.g.have_nerd_font }
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
     require('mini.cursorword').setup()
     require('mini.animate').setup()
   end,
