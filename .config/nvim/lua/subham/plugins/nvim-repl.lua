57cac2bc1573a3c16f6de377a030696a89ce06ef
subhamc88 <subham020304@gmail.com>
2024-07-15 18:47:09 +0530

    some neovim plugins added related to machine learning

---
+++ .config/nvim/lua/subham/plugins/nvim-repl.lua
@@ -0,0 +1,13 @@
+return {
+  'pappasam/nvim-repl',
+  init = function()
+    vim.g['repl_filetype_commands'] = {
+      javascript = 'node',
+      python = 'ipython --no-autoindent',
+    }
+  end,
+  keys = {
+    { '<leader>rt', '<cmd>ReplToggle<cr>', desc = 'Toggle nvim-repl' },
+    { '<leader>rc', '<cmd>ReplRunCell<cr>', desc = 'nvim-repl run cell' },
+  },
+}
