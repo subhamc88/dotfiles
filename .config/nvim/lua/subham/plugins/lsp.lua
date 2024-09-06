501a53b9ee1a226335fa8acbc011b588f0a5dff9
subhamc88 <subham020304@gmail.com>
2024-09-06 10:32:04 +0530

    removed tsserver because deprecated

--- .config/nvim/lua/subham/plugins/lsp.lua
+++ .config/nvim/lua/subham/plugins/lsp.lua
@@ -54,7 +54,6 @@ return {
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
     local servers = {
-      tsserver = {},
       lua_ls = {
         settings = {
           Lua = {
