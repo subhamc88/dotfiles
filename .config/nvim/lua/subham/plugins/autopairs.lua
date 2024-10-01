b8d076dcd49cc1847687fdabdc4f463a5cb795c3
subhamc88 <subham020304@gmail.com>
2024-10-02 00:43:22 +0530

    changed autopairs.lua

--- .config/nvim/lua/subham/plugins/autopairs.lua
+++ .config/nvim/lua/subham/plugins/autopairs.lua
@@ -1,7 +1,6 @@
 return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
-  lazy = true,
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
     require('nvim-autopairs').setup {}
