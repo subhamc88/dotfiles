637b571b1a2bfbd4a87f77ea8a5cf4ee75270734
subhamc88 <subham020304@gmail.com>
2024-10-04 18:27:28 +0530

    removed neovim config

--- .config/nvim/lua/subham/plugins/colorizer.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'norcalli/nvim-colorizer.lua',
-  lazy = true,
-  config = function()
-    require('colorizer').setup()
-  end,
-}
--- .config/nvim/lua/subham/plugins/render-markdown.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  main = 'render-markdown',
-  event = 'VeryLazy',
-  opts = {},
-  name = 'render-markdown',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
-}
