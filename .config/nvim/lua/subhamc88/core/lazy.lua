7fb3060d824efbe76ff128f43d2d9917aa8ca996
subhamc88 <subham020304@gmail.com>
2024-10-04 18:38:22 +0530

    added lazy.lua

---
+++ .config/nvim/lua/subhamc88/core/lazy.lua
@@ -0,0 +1,36 @@
+--== Lazy Package Manager==--
+
+-- Space as Leader Key. --
+vim.g.mapleader = " "
+
+-- Bootstrap lazy package manager. --
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Setup lazy --
+require("lazy").setup({
+	spec = {
+		-- Importing plugins
+		{
+			import = "subhamc88.plugins",
+			-- By default plugins will be lazy loaded.
+			lazy = true,
+		},
+	},
+	-- Setting up colorscheme --
+	install = { colorscheme = { "catppuccin-mocha", "habamax" } },
+	checker = { enabled = true },
+})
