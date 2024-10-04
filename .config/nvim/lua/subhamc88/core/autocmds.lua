b3d0ec1f173c3e4f863bc84c5085c4695dd39523
subhamc88 <subham020304@gmail.com>
2024-10-04 18:31:21 +0530

    added autocmds.lua

---
+++ .config/nvim/lua/subhamc88/core/autocmds.lua
@@ -0,0 +1,9 @@
+-- Highlight for a short time when yanking.
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
