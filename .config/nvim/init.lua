09aca5be92e6feda73268c61a07bbaee34e80c94
subhamc88 <subham020304@gmail.com>
2024-10-04 19:13:25 +0530

    updated init.lua to make sure new config is loaded

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
-require 'subham.core.opts'
-require 'subham.core.lazy'
-require 'subham.core.keymaps'
+require('subhamc88.core.lazy')
+require('subhamc88.core.options')
+require('subhamc88.core.keymaps')
+require('subhamc88.core.autocmds')
