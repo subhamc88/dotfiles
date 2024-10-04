ac06a48d6d99470858b0b60abfd83e2e8bb6dab8
subhamc88 <subham020304@gmail.com>
2024-10-04 19:07:03 +0530

    added format.lua for formatting

---
+++ .config/nvim/lua/subhamc88/plugins/format.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  event = "VimEnter",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "blackd-client" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        typescript = { "prettierd", stop_after_first = true },
+        markdown = { "markdownlint-cli2" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        aync = false,
+        timeout_ms = 500,
+      },
+    })
+  end,
+}
