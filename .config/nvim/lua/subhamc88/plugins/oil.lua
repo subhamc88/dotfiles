a7c9bfd06ea954435a04055e4597c8e6e6895109
subhamc88 <subham020304@gmail.com>
2024-10-04 19:08:59 +0530

    added oil.lua for editing file system in buffer

---
+++ .config/nvim/lua/subhamc88/plugins/oil.lua
@@ -0,0 +1,20 @@
+return {
+  'stevearc/oil.nvim',
+  event = "InsertEnter",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = false,
+      view_options = {
+        show_hidden = false,
+        is_hidden_file = function(name, bufnr)
+          return vim.startswith(name, ".")
+        end,
+        natural_order = true,
+        case_insensitive = false,
+      }
+
+    })
+  end,
+}
