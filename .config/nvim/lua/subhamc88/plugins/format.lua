5e2bd715055291a8112aadd365dbaa8b69f8d4a2
subhamc88 <subham020304@gmail.com>
2024-10-08 11:38:29 -0400

    updated format.lua with go formatting options added

--- .config/nvim/lua/subhamc88/plugins/format.lua
+++ .config/nvim/lua/subhamc88/plugins/format.lua
@@ -1,20 +1,21 @@
 return {
-  "stevearc/conform.nvim",
-  event = "VimEnter",
-  config = function()
-    require("conform").setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "isort", "blackd-client" },
-        rust = { "rustfmt", lsp_format = "fallback" },
-        typescript = { "prettierd", stop_after_first = true },
-        markdown = { "markdownlint-cli2" },
-      },
-      format_on_save = {
-        lsp_fallback = true,
-        aync = false,
-        timeout_ms = 500,
-      },
-    })
-  end,
+	"stevearc/conform.nvim",
+	event = "VimEnter",
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "isort", "blackd-client" },
+				rust = { "rustfmt", lsp_format = "fallback" },
+				typescript = { "prettierd", stop_after_first = true },
+				markdown = { "markdownlint-cli2" },
+				go = { "goimports", "gomodifytags", "goimports-reviser", "golines", "go-tests", "gofmupt" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				aync = false,
+				timeout_ms = 500,
+			},
+		})
+	end,
 }
