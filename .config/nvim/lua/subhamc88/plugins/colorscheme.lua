212a04a0785fa754538f99679718426e1e323145
subhamc88 <subham020304@gmail.com>
2024-11-05 16:27:16 +0000

    fix(nvim): change colorscheme.lua
    - Colorscheme changed to gruvbox.

--- .config/nvim/lua/subhamc88/plugins/colorscheme.lua
+++ .config/nvim/lua/subhamc88/plugins/colorscheme.lua
@@ -1,11 +1,8 @@
-return {
-  'maxmx03/solarized.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function(_, opts)
-    vim.o.termguicolors = true
-    vim.o.background = 'dark'
-    require('solarized').setup(opts)
-    vim.cmd.colorscheme 'solarized'
-  end,
+return{
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000 ,
+  config = function ()
+    vim.cmd("colorscheme gruvbox")
+  end
 }
+
