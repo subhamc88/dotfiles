d93ba45aa865e96237bc4f2cff65349abb47a402
subhamc88 <subham020304@gmail.com>
2024-11-07 07:02:37 +0000

    fix(script): changed alias.sh
    - Fixed bugs and improved the repos function.

--- .config/shell/core/alias.sh
+++ .config/shell/core/alias.sh
@@ -5,12 +5,10 @@ alias ll="exa -la --group-directories-first --color=always"
 
 # Autojump
 j() {
-	local dir
-	dir="$(autojump "$1")"
-	cd "$dir" || echo "Directory not found."
+	cd "$2/$1" || echo "Directory not found."
 }
 
 # Repository List
 repos() {
-	j "$(exa -l /storage/projects/repos | awk '{print $7}' | fzf)"
+	j "$(exa -l /storage/projects/repos | awk '{print $7}' | fzf)" "/storage/projects/repos"
 }
